da61282fcc3fc978bf877745268a64c0679feef3
Janet Leon <leon.irenejanet@gmail.com>
2018-09-18 12:54:24 -0700

    updated scratch file to search with

--- scratch/queries.js
+++ scratch/queries.js
@@ -4,19 +4,22 @@ const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-// .then(()=>{
-//     return Note.find();
-// })
-//   .then(() => {
-//     const searchTerm = new RegExp('lady gaga', 'gi');
-//     let filter = {};
+.then(()=>{
+    return Note.find();
+})
+  .then(() => {
+    const searchTerm = new RegExp('lady gaga', 'gi');
+    let filterTitle = {};
+    let filterContent= {};
 
-//     if (searchTerm) {
-//       filter.title = { $regex: searchTerm };
-//     }
 
-//     return Note.find(filter).sort({ updatedAt: 'desc' });
-//   })
+    if (searchTerm) {
+      filterTitle.title = { $regex: searchTerm };
+      filterContent.content = { $regex: searchTerm };
+    }
+
+    return Note.find({$or: [filterTitle, filterContent]}).sort({ updatedAt: 'desc' });
+  })
 // .then(()=>{
 //     const id = "000000000000000000000005";
 //     return Note.findById(id);
@@ -27,14 +30,19 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
 
 //     return Note.create(newNote);
 // })
-.then(()=>{
-    const updateId = "000000000000000000000005";
-    const updatedNote = {
-        title: "French Bull Dogs are too cute",
-        content: "They have lots of nasal problems"
-    };
-    return Note.findByIdAndUpdate(updateId, {$set: updatedNote});
-})
+// .then(()=>{
+//     const updateId = "000000000000000000000005";
+//     const updatedNote = {
+//         title: "French Bull Dogs are too cute",
+//         content: "They have lots of nasal problems"
+//     };
+//     return Note.findByIdAndUpdate(updateId, {$set: updatedNote});
+// })
+// .then(()=>{
+//     const deleteId = "5ba154aad75ca91b2888c71a";
+
+//     return Note.findByIdAndRemove(deleteId);
+// })
   .then(results => {
     console.log(results);
   })
