f5acd3fdba1b455b0493b573ba6bf0d860f7d361
Janet Leon <leon.irenejanet@gmail.com>
2018-09-20 11:58:39 -0700

    created feature branch. Created folder.js file to house folderSchema

---
+++ models/folder.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const folderSchema = new mongoose.Schema({
+  title: {type: String, required: true, unique: true},
+
+});
+
+folderSchema.set('timestamps', true);
+
+folderSchema.set('toObject', {
+  virtual: true,
+  transform: function(doc,ret){
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+module.exports = mongoose.model('Folder', folderSchema);
