980be7c92e98ad6bcce74aa8ac4bc6ccc6d03256
Janet Leon <leon.irenejanet@gmail.com>
2018-09-24 12:05:39 -0700

    Router for /api/users

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+'use strict';
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const { MONGODB_URI } = require('../config');
+
+
+
+
+
+
+
+
+//====================================
+module.exports = router;
