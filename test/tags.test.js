3b50afbbd7d90897539597472f53334d165035c2
Janet Leon <leon.irenejanet@gmail.com>
2018-09-27 17:48:40 -0700

    updated tags tests to allow for authentication and jwts

--- test/tags.test.js
+++ test/tags.test.js
@@ -1,16 +1,14 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
-
+const jwt = require('jsonwebtoken');
 const app = require('../server');
-const { TEST_MONGODB_URI} = require('../config');
-
+const { TEST_MONGODB_URI, JWT_SECRET} = require('../config');
+const {notes,tags, folders, users} = require('../db/seed/data');
 const Note = require('../models/note');
-const Folder = require('../models/folder');
 const Tag = require('../models/tags');
-
-const {notes, folders, tags} = require('../db/seed/data');
-
+const Folder = require('../models/folder');
+const User = require('../models/user');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
@@ -35,12 +33,22 @@ describe('Connect, createDb, read/update/delete from db, drobDb, disconnect',
         .then(() => mongoose.connection.db.dropDatabase());
     });
   
+    let token;
+    let user;
+
     beforeEach(function () {
       return Promise.all([
+        User.insertMany(users),
+        Folder.insertMany(folders),
+        Folder.createIndexes(),
         Tag.insertMany(tags),
         Tag.createIndexes(),
-        Note.insertMany(notes),
-      ]);
+        Note.insertMany(notes)
+      ])
+        .then(([users]) => {
+          user = users[0];
+          token = jwt.sign({ user }, JWT_SECRET, { subject: user.username });
+        });
     });
   
     afterEach(function () {
@@ -56,11 +64,12 @@ describe('Connect, createDb, read/update/delete from db, drobDb, disconnect',
       it('Get All Tags', function(){
         return chai.request(app)
           .get('/api/tags')
+          .set('Authorization', `Bearer ${token}`)
           .then(response=>{
             expect(response).to.be.json;
             expect(response.body).to.be.a('array');
             expect(response.body[0]).to.be.a('object');
-            expect(response.body[0]).to.have.keys('name', 'createdAt', 'updatedAt', 'id');
+            expect(response.body[0]).to.have.keys('name', 'createdAt', 'updatedAt', 'id', 'userId');
           });
       });
       
@@ -76,14 +85,15 @@ describe('Connect, createDb, read/update/delete from db, drobDb, disconnect',
         return Tag.findOne()
           .then(result => {
             tag = result;
-            return chai.request(app).get(`/api/tags/${tag.id}`);
+            return chai.request(app).get(`/api/tags/${tag.id}`)
+              .set('Authorization', `Bearer ${token}`);
           })
           .then((res) => {
             expect(res).to.be.json;
             expect(res.body).to.be.an('object');
             expect(res.body.id).to.equal(tag.id);
             expect(res.body.name).to.equal(tag.name);
-            expect(res.body).to.have.all.keys('id', 'name', 'createdAt', 'updatedAt');
+            expect(res.body).to.have.all.keys('id', 'name', 'createdAt', 'updatedAt', 'userId');
             expect(new Date(res.body.createdAt)).to.deep.equal(tag.createdAt);
             expect(new Date(res.body.updatedAt)).to.deep.equal(tag.updatedAt);
             expect(res).to.have.status(200);
@@ -98,7 +108,8 @@ describe('Connect, createDb, read/update/delete from db, drobDb, disconnect',
       it('create and return new tag', function(){
         const newTag = {name: "BrandNewTag"};
         let tagRes;
-        chai.request(app).post('/api/tags')
+        return chai.request(app).post('/api/tags')
+          .set('Authorization', `Bearer ${token}`)
           .send(newTag)
           .then(response=>{
             tagRes= response;
@@ -106,34 +117,38 @@ describe('Connect, createDb, read/update/delete from db, drobDb, disconnect',
             expect(response).to.have.status(200);
             expect(response).to.have.header('location');
            
-            //console.log(response);
+            // console.log(response.body);
             expect(response.header.location).to.equal(tagRes.header.location);
             expect(response.body.name).to.equal(newTag.name);
             expect(new Date(response.body.createdAt)).to.not.equal(null);
             expect(new Date(response.body.updatedAt)).to.not.equal(null);
-            expect(response.body).to.have.keys('id', 'name', 'createdAt', 'updatedAt');
-            return Tag.findById(tagRes.body.id);
-          })
-          .then((tagDbRes)=>{
-            expect(tagDbRes.id).to.equal(tagRes.body.id);
-            expect(tagDbRes.name).to.equal(tagRes.body.name);
+            expect(response.body).to.have.keys('id', 'name', 'createdAt', 'updatedAt', 'userId');
+            return Tag.find({userId: user.id, _id:tagRes.body.id})
+              .set('Authorization', `Bearer ${token}`)
+              .then(([tagDbRes])=>{
+                // console.log(tagDbRes);
+                expect(tagDbRes.id).to.equal(tagRes.body.id);
+                expect(tagDbRes.name).to.equal(tagRes.body.name);
+              });
           });
+          
       });
     });
 
     /*==================UDATE/PUT api/tags by ID====================*/
 
-    describe('update a tag\'s fields by id number', function(){
+    describe('PUT api/tags:id', function(){
       it('update correct tag located its id, and return updated tag', function(){
         const updateTag = {name: 'updated name test'};
       
         let dbTag;
         return Tag
-          .findOne()
+          .findOne({userId: user.id})
           .then(function(dbRes) {
             dbTag= dbRes;
             return chai.request(app)
               .put(`/api/tags/${dbRes.id}`)
+              .set('Authorization', `Bearer ${token}`)
               .send(updateTag);
           })
           .then((res)=>{
@@ -143,7 +158,8 @@ describe('Connect, createDb, read/update/delete from db, drobDb, disconnect',
             expect(res.body.name).to.equal(updateTag.name);
             expect(new Date(res.body.createdAt)).to.eql(new Date(dbTag.createdAt));
             return chai.request(app)
-              .get(`/api/tags/${dbTag.id}`);
+              .get(`/api/tags/${dbTag.id}`)
+              .set('Authorization', `Bearer ${token}`);
           })
           .then((chaiRes)=>{
             expect(chaiRes).to.have.status(200);
@@ -168,14 +184,16 @@ describe('Connect, createDb, read/update/delete from db, drobDb, disconnect',
         let tag;
   
         return Tag
-          .findOne()
+          .findOne({userId: user.id})
           .then(dbRes=>{
             tag = dbRes;
-            return chai.request(app).delete(`/api/tags/${dbRes.id}`);
+            return chai.request(app).delete(`/api/tags/${dbRes.id}`)
+              .set('Authorization', `Bearer ${token}`);
           })
           .then((response)=>{
             expect(response).to.have.status(204);
-            return chai.request(app).get(`/api/tags/${tag.id}`);
+            return chai.request(app).get(`/api/tags/${tag.id}`)
+              .set('Authorization', `Bearer ${token}`);
           })
           .then(response=>{
             expect(response.body).to.be.null;
